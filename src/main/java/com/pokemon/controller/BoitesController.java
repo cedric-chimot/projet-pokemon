0b161e65b0daea2130b26ff68ed9bffd91a31047
Cedric Chimot <cedric_om10@yahoo.fr>
2024-12-22 22:16:30 +0100

    méthodes pour récupérer les stats pour chaque catégorie

--- src/main/java/com/pokemon/controller/BoitesController.java
+++ src/main/java/com/pokemon/controller/BoitesController.java
@@ -9,34 +9,92 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * Le controller des boites
+ */
 @RestController
 @RequestMapping("/api/boites")
 @CrossOrigin
 public class BoitesController {
 
+    /**
+     * Variable pour appeler le service
+     */
     private final BoiteService boiteService;
 
+    /**
+     * Le constructeur du controller
+     * @param boiteService Injection du service
+     */
     public BoitesController(BoiteService boiteService) {
         this.boiteService = boiteService;
     }
 
+    /**
+     * Afficher toutes les boites
+     * @return la liste des boites
+     */
     @GetMapping
     public List<Boites> getAllBoites() {
         return boiteService.getAllBoites();
     }
 
+    /**
+     * Rechercher une boite par son id
+     * @param id l'id de la boite
+     * @return la boite trouvée
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Boites> getBoiteById(@PathVariable Integer id) {
         Optional<Boites> boite = boiteService.getBoiteById(id);
         return boite.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/{boiteId}/stats")
-    @ResponseBody
-    public Map<String, List<Map<String, Object>>> getStats(@PathVariable Integer boiteId) {
-        return boiteService.getStatsByBoite(boiteId);
+    /**
+     * Afficher la liste des stats d'une boite
+     * @param boiteId l'id de la boite recherchée
+     * @return la boite trouvée et ses statistiques
+     */
+    @GetMapping("/{boiteId}/all-stats")
+    public ResponseEntity<Map<String, Object>> getAllStats(@PathVariable Integer boiteId) {
+        return ResponseEntity.ok(boiteService.getAllStats(boiteId));
     }
 
+    @GetMapping("/{boiteId}/dresseurs")
+    public ResponseEntity<List<Map<String, Object>>> getDresseursStats(@PathVariable Integer boiteId) {
+        List<Map<String, Object>> stats = boiteService.getDresseursStats(boiteId);
+        return ResponseEntity.ok(stats);
+    }
+
+    @GetMapping("/{boiteId}/pokeballs")
+    public ResponseEntity<List<Map<String, Object>>> getPokeballsStats(@PathVariable Integer boiteId) {
+        List<Map<String, Object>> stats = boiteService.getPokeballsStats(boiteId);
+        return ResponseEntity.ok(stats);
+    }
+
+    @GetMapping("/{boiteId}/natures")
+    public ResponseEntity<List<Map<String, Object>>> getNaturesStats(@PathVariable Integer boiteId) {
+        List<Map<String, Object>> stats = boiteService.getNaturesStats(boiteId);
+        return ResponseEntity.ok(stats);
+    }
+
+    @GetMapping("/{boiteId}/sexes")
+    public ResponseEntity<List<Map<String, Object>>> getSexesStats(@PathVariable Integer boiteId) {
+        List<Map<String, Object>> stats = boiteService.getSexesStats(boiteId);
+        return ResponseEntity.ok(stats);
+    }
+
+    @GetMapping("/{boiteId}/types")
+    public ResponseEntity<List<Map<String, Object>>> getTypesStats(@PathVariable Integer boiteId) {
+        List<Map<String, Object>> stats = boiteService.getTypesStats(boiteId);
+        return ResponseEntity.ok(stats);
+    }
+
+    /**
+     * Créer une nouvelle boite
+     * @param boite la boite à créer
+     * @return la nouvelle boite
+     */
     @PostMapping("/create")
     public ResponseEntity<Boites> createBoite(@RequestBody Boites boite) {
         Boites savedBoite = boiteService.save(boite);
@@ -44,9 +102,9 @@ public class BoitesController {
     }
 
     /**
-     * Mettre à jour un boite
-     * @param boite le boite à mettre à jour
-     * @return le boite mis à jour
+     * Mettre à jour une boite
+     * @param boite la boite à mettre à jour
+     * @return la boite mise à jour
      */
     @PatchMapping("/update")
     public Boites updateboite(@RequestBody Boites boite) {
@@ -64,7 +122,7 @@ public class BoitesController {
     }
 
     /**
-     * Supprimer tous les boites
+     * Supprimer toutes les boites
      */
     @DeleteMapping("/delete/all")
     public void deleteAllShinies() {
--- src/main/java/com/pokemon/service/BoiteService.java
+++ src/main/java/com/pokemon/service/BoiteService.java
@@ -12,29 +12,62 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+/**
+ * Service pour gérer les méthodes des boites
+ */
 @Service
 @Transactional
 public class BoiteService {
+
+    /**
+     * Le repository des boites
+     */
     private final BoitesRepository boitesRepository;
 
+    /**
+     * Constructeur du service
+     * @param boitesRepository Injection du repository des boites
+     */
     public BoiteService(BoitesRepository boitesRepository) {
         this.boitesRepository = boitesRepository;
     }
 
+    /**
+     * Méthode pour créer une nouvelle boite
+     * @param boite la boite à créer
+     * @return la boite nouvellement créée
+     */
     public Boites save(Boites boite) {
         return boitesRepository.save(boite);
     }
-    
+
+    /**
+     * Afficher toutes les boites
+     * @return la liste des boites
+     */
     public List<Boites> getAllBoites() {
         return boitesRepository.findAll();
     }
-    
+
+    /**
+     * Trouver une boite avec son id
+     * @param id l'id de la boite recherchée
+     * @return la boite trouvée et toutes ses données
+     */
     public Optional<Boites> getBoiteById(Integer id) {
         return boitesRepository.findById(id);
     }
 
-    public Map<String, List<Map<String, Object>>> getStatsByBoite(Integer boiteId) {
-        Map<String, List<Map<String, Object>>> stats = new HashMap<>();
+    /**
+     * Récupère les statistiques d'une boîte spécifiée en fonction de l'ID de la boîte.
+     * Les statistiques incluent les données sur les Pokéballs, Dresseurs, Sexes, Natures et Types.
+     *
+     * @param boiteId L'ID de la boîte dont on veut récupérer les statistiques.
+     * @return Un map contenant des statistiques sous forme de liste pour chaque catégorie :
+     *         Pokéballs, Dresseurs, Sexes, Natures, Types.
+     */
+    public Map<String, Object> getAllStats(Integer boiteId) {
+        Map<String, Object> stats = new HashMap<>();
 
         stats.put("Pokeballs", mapStats(boitesRepository.findStatsByPokeball(boiteId)));
         stats.put("Dresseurs", mapStats(boitesRepository.findStatsByDresseur(boiteId)));
@@ -45,6 +78,40 @@ public class BoiteService {
         return stats;
     }
 
+    public List<Map<String, Object>> getDresseursStats(Integer boiteId) {
+        List<Object[]> results = boitesRepository.findStatsByDresseur(boiteId);
+        return mapStats(results);
+    }
+
+    public List<Map<String, Object>> getPokeballsStats(Integer boiteId) {
+        List<Object[]> results = boitesRepository.findStatsByPokeball(boiteId);
+        return mapStats(results);
+    }
+
+    public List<Map<String, Object>> getNaturesStats(Integer boiteId) {
+        List<Object[]> results = boitesRepository.findStatsByNature(boiteId);
+        return mapStats(results);
+    }
+
+    public List<Map<String, Object>> getSexesStats(Integer boiteId) {
+        List<Object[]> results = boitesRepository.findStatsBySexe(boiteId);
+        return mapStats(results);
+    }
+
+    public List<Map<String, Object>> getTypesStats(Integer boiteId) {
+        List<Object[]> results = boitesRepository.findStatsByType(boiteId);
+        return mapStats(results);
+    }
+
+    /**
+     * Transforme une liste de résultats sous forme d'objets en une liste de maps contenant les données correspondantes.
+     * Cette méthode traite différemment les cas où les résultats contiennent 2 ou 3 éléments :
+     * Si le résultat contient trois éléments, c'est un dresseur et les données sont mappées avec "idDresseur", "dresseur" et "nbPokemon".
+     * Si le résultat contient deux éléments, ce sont d'autres statistiques et les données sont mappées avec "name" et "nbPokemon".
+     *
+     * @param results Une liste d'objets représentant les résultats récupérés (par exemple depuis une requête SQL).
+     * @return Une liste de maps, chaque map contenant les informations mappées selon la structure des résultats.
+     */
     public List<Map<String, Object>> mapStats(List<Object[]> results) {
         return results.stream()
                 .map(result -> {
@@ -94,11 +161,10 @@ public class BoiteService {
     }
 
     /**
-     * Supprimer tous les boites
+     * Supprimer toutes les boites
      */
     public void deleteAll() {
         boitesRepository.deleteAll();
     }
-
 
 }
